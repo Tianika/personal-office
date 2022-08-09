47df9173a4f90441b6c9d2aba4559feb625b18c4
Tianika <tatyriana@yandex.ru>
2022-08-09 12:13:17 +0300

    init: basic configuration

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # personal-office
+
+# Downloading
+git clone https://github.com/Tianika/personal-office.git
+
+# Client
+$ cd personal-office
+
+$ npm install
+
+$ npm start
+
+Node v16.16.0
---
+++ personal-office/src/index.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/redux/store/store';
+import App from './app/App';
+import './index.css';
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
