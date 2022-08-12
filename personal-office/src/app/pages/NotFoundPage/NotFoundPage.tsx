2c122987a71eaa3c036475101d0e5310a041b794
Tianika <tatyriana@yandex.ru>
2022-08-12 14:41:34 +0300

    feat: add not found page style

--- personal-office/src/app/pages/NotFoundPage/NotFoundPage.tsx
+++ personal-office/src/app/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,12 @@
 import styles from './styles.module.scss';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.title}>404</div>
+      <div className={styles.text}>Not found</div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
